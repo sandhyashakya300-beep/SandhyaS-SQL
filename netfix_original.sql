a6506528364f58a8222baf1c125f2cf14d12a6be
Sandhya Shakya <sandhyashakya300@gmail.com>
2026-09-11 18:00:17 +0530

    Add files via upload

---
+++ netfix_original.sql
@@ -0,0 +1,10 @@
+create TABLE netflix_originals(
+movie_name varchar(233) NOT NULL, 
+genre_ID INT,
+genre_name varchar(233),
+
+runtime TIME,
+IMDb_score int,
+language_ varchar(122),
+Premiere_date DATE);
+ 
